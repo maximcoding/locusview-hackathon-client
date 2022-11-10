d06e750ec59c5fd9149dc5ec4340c1612f558a74
amirlocusview <amir.algazi@locusview.com>
2022-11-10 12:27:09 +0200

    Almost final

---
+++ src/Home.tsx
@@ -0,0 +1,24 @@
+import { Route, BrowserRouter as Router, Routes, Link } from 'react-router-dom';
+import App from './App';
+import Profile from './Profile';
+
+const Home = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/" element={<App />} />
+        </Routes>
+        <div>
+          <h1>My App</h1>
+          <nav>
+            <Link to="/">Home</Link> | <Link to="profile">About</Link>
+          </nav>
+        </div>
+      </Router>
+    </div>
+  );
+};
+
+export default Home;
